175d2d3fbeac64abdda771036a9aca11757bc89f
Shashank <shashank0304@gmail.com>
2023-05-17 23:26:13 +0530

    Add mockito matchers examples - 17/05/2023

--- src/test/java/com/ssangu/business/ListTest.java
+++ src/test/java/com/ssangu/business/ListTest.java
@@ -1,9 +1,11 @@
 package com.ssangu.business;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -65,5 +67,26 @@ class ListTest {
 		Assertions.assertThrows(RuntimeException.class,()->listMock.get(0));
 	}
 	
-
+	@Test
+	public void letsMockList_MatchersWithHardCodedValue() {
+		
+		List listMock = mock(List.class);
+		//when(listMock.subList(anyInt(), 5)).thenThrow(new RuntimeException("Testing Mockito with matchers with hardcoded value"));
+		//Assertions.assertThrows(RuntimeException.class, ()->listMock.subList(0, 5));
+	}
+	
+	@Test
+	public void letMockListGetMethod_usingBDD() {
+		
+		@SuppressWarnings("unchecked")
+		//Given
+		List<String> listMock = mock(List.class);
+		given(listMock.get(0)).willReturn("Shashank");
+		
+		//When
+		String firstElement = listMock.get(0);
+		
+		//Then
+		assertThat(firstElement, is("Shashank"));
+	}
 }
--- src/test/java/com/ssangu/business/TodoBusinessImplMock.java
+++ src/test/java/com/ssangu/business/TodoBusinessImplMock.java
@@ -1,6 +1,9 @@
 package com.ssangu.business;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -11,16 +14,24 @@ import org.junit.jupiter.api.Test;
 
 import com.ssangu.data.api.TodoService;
 
+
+
+
 public class TodoBusinessImplMock {
 	
 	@Test
 	public void testRetrieveTodosRelatedToSpring_usingAMock() {
 		
+		//Given
 		TodoService todoServiceMock = mock(TodoService.class);
 		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(Arrays.asList("Learn Spring","Learn to eat","Learn Spring MVC"));
+		
 		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
 		
+		//When
+		
 		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
+		//Then
 		assertEquals(2, filteredTodos.size());
 	}
 	
@@ -33,6 +44,22 @@ public class TodoBusinessImplMock {
 		
 		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
 		assertEquals(0, filteredTodos.size());
+	}
+	
+	@Test
+	public void testRetrieveTodosRelatedToSpring_usingBDDMock() {
+		
+		//Given
+		TodoService todoServiceMock = mock(TodoService.class);
+		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(Arrays.asList("Learn Spring","Learn to eat","Learn Spring MVC"));
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		
+		//When
+		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
+		
+		//Then
+		assertThat(filteredTodos.size(), is(2));
+		
 	}
 }
 
